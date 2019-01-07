b413384113f42f58963d1af1df86ad480da7950e
SIDDHANT JAIN <32744277+jain-sidhnt@users.noreply.github.com>
2019-01-07 20:12:19 +0530

    Peterson Algorithm
    C++ code to implement peterson algorithm in operating system.

---
+++ peterson.cpp
@@ -0,0 +1,70 @@
+#include <stdio.h> 
+
+int flag[2]; 
+int turn; 
+const int MAX = 1e9; 
+int ans = 0; 
+  
+void lock_init() 
+{ 
+    
+    flag[0] = flag[1] = 0; 
+    turn = 0; 
+} 
+  
+// Executed before entering critical section 
+void lock(int self) 
+{ 
+    // Set flag[self] = 1 saying you want to acquire lock 
+    flag[self] = 1; 
+  
+    turn = 1-self; 
+  
+    while (flag[1-self]==1 && turn==1-self) ; 
+} 
+  
+// Executed after leaving critical section 
+void unlock(int self) 
+{ 
+    
+    flag[self] = 0; 
+} 
+  
+// A Sample function run by two threads created  
+// in main() 
+void* func(void *s) 
+{ 
+    int i = 0; 
+    int self = (int *)s; 
+    printf("Thread Entered: %d\n", self); 
+  
+    lock(self); 
+  
+    // Critical section (Only one thread 
+    // can enter here at a time) 
+    for (i=0; i<MAX; i++) 
+        ans++; 
+  
+    unlock(self); 
+} 
+  
+// Driver code 
+int main() 
+{ 
+    // Initialized the lock then fork 2 threads 
+    pthread_t p1, p2; 
+    lock_init(); 
+  
+    // Create two threads (both run func)  
+    pthread_create(&p1, NULL, func, (void*)0); 
+    pthread_create(&p2, NULL, func, (void*)1); 
+  
+    // Wait for the threads to end. 
+    pthread_join(p1, NULL); 
+    pthread_join(p2, NULL); 
+  
+    printf("Actual Count: %d | Expected Count: %d\n", 
+                                        ans, MAX*2); 
+  
+    return 0; 
+} 
